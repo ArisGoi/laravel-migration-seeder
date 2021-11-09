dd1bdc6ce00e1a558296a2986d2a5a29704e9c52
ArisGoi <aris.goi97@gmail.com>
2021-11-09 16:14:20 +0100

    first commit

---
+++ database/migrations/2021_11_09_144712_create_packages_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePackagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('packages', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 80);
+            $table->text('description');
+            $table->tinyInteger('n_people');
+            $table->smallInteger('n_location');
+            $table->tinyInteger('duration_days');
+            $table->smallInteger('validity_days');
+            $table->float('stars', 2, 1);
+            $table->float('price', 10, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('packages');
+    }
+}
